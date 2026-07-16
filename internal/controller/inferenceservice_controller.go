c37b967c01095a9c048cd21b92049a8ac6d878d3
lbl <2388522369@qq.com>
2026-07-17 02:32:34 +0800

    feat(controller):code method of  buildDeployment and reconcile

--- internal/controller/inferenceservice_controller.go
+++ internal/controller/inferenceservice_controller.go
@@ -19,12 +19,18 @@ package controller
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	aiv1 "github.com/2388522369/inference-operator/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // InferenceServiceReconciler reconciles a InferenceService object
@@ -47,11 +53,74 @@ type InferenceServiceReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
 func (r *InferenceServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var infService aiv1.InferenceService
+	if err := r.Get(ctx, req.NamespacedName, &infService); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	var deploy appsv1.Deployment
+	err := r.Get(ctx, types.NamespacedName{Name: infService.Name, Namespace: infService.Namespace}, &deploy)
+	if err != nil && errors.IsNotFound(err) {
+		log.Info("Deployment not found, creating...")
+
+		desiredDeploy := r.buildDeployment(&infService)
+		if err := r.Create(ctx, desiredDeploy); err != nil {
+			log.Error(err, "Failed to create Deployment")
+			return ctrl.Result{}, err
+		}
+
+		log.Info("Deployment created sucessfully")
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return ctrl.Result{}, err
+	}
+
+	infService.Status.Phase = "Pending"
+	if err := r.Status().Update(ctx, &infService); err != nil {
+		log.Error(err, "Failed to update status")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *InferenceServiceReconciler) buildDeployment(inf *aiv1.InferenceService) *appsv1.Deployment {
+	labels := map[string]string{
+		"app":       "inference",
+		"modelName": inf.Spec.ModelName,
+	}
+
+	deploy := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      inf.Name,
+			Namespace: inf.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &inf.Spec.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "inference-server",
+							Image: inf.Spec.Image,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_ = controllerutil.SetControllerReference(inf, deploy, r.Scheme)
+
+	return deploy
 }
 
 // SetupWithManager sets up the controller with the Manager.
